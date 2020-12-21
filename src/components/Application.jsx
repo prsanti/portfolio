7f53b0b222021840acf28b852f95065c02213576
Paulo Santiago <p.santiago796@gmail.com>
2020-12-21 17:44:45 -0500

    create Application component and style

---
+++ src/components/Application.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Application.scss'
+
+export default function Application() {
+  return (
+    <main className="layout">
+      <p>testing</p>
+    </main>
+  );
+}
