2fc6f1a548c5694899232734b08b2c915879f58b
Paulo Santiago <p.santiago796@gmail.com>
2020-12-21 22:05:29 -0500

    create Header and About components

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './About.scss';
+
+export default function About() {
+  return (
+    <h1 className="full-name">
+      <div className="first-name">Paulo</div>
+      <div className="last-name">Santiago</div>
+    </h1>
+  );
+}
--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Application.scss'
+import Header from "./Header.jsx";
+import About from "./About.jsx";
 
 export default function Application() {
   return (
+
     <main className="layout">
+      <About/>
       <p>testing</p>
     </main>
   );
